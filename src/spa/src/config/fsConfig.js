d387cdcdb5c06c27c8853e33be023af834ba65cd
Paul <35410179+yaiestura@users.noreply.github.com>
2019-02-17 02:23:10 +0300

    edit Firebase config file

--- src/spa/src/config/fsConfig.js
+++ src/spa/src/config/fsConfig.js
@@ -3,14 +3,8 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
  // Initialize Firebase
- var config = {
-    apiKey: "AIzaSyBACyT_OlUX-Ap4lhzF22lX8OPnVeau8ZQ",
-    authDomain: "onviftester.firebaseapp.com",
-    databaseURL: "https://onviftester.firebaseio.com",
-    projectId: "onviftester",
-    storageBucket: "onviftester.appspot.com",
-    messagingSenderId: "300299386420"
-  };
+
+var config = { /*your Firebase config and API key here*/ };
 firebase.initializeApp(config);
 firebase.firestore().settings({ timestampsInSnapshots: true });
 
