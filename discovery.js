ecbafec1e2e1b8c704d37365134a58972d5f7d90
Artemy <magar.art@yandex.ru>
2019-02-16 16:33:59 +0300

    prettify discovery.js

--- discovery.js
+++ discovery.js
@@ -1,18 +1,27 @@
 const onvif = require("node-onvif");
-var fs = require("fs");
+const fs    = require("fs");
+
+function parseIpPort(s) {
+  let port = s.match(/(?<=:)\d+/)[0];
+  port = port == null ? 80 : port;
+  let ip = s.match(/(\d+\.){3}\d+/)[0];
+  return { ip: ip, port: port };
+}
+
+function parseDevice(it, id) {
+  return { id: id, ...parseIpPort(it) }
+}
 
 onvif.startProbe().then((devices) => {
   let mapped = devices.map(info => info.xaddrs[0]);
-  var json = [];
-  mapped.map((it, id) => json.push({id: id, ip: it.replace('http://','')
-  .replace('/onvif/device_service','').split(':')[0],
-  port: typeof(it.replace('http://','').replace('/onvif/device_service','').split(':')[1]) !== 'undefined' ? it.replace('http://','').replace('/onvif/device_service','').split(':')[1] : '80'}));
+  let json = mapped.map(parseDevice);
   console.log(json);
+
   fs.writeFile("./devices.json", JSON.stringify(json), (err) => {
-    if (err) {
-        console.error(err);
-        return;
-    };
-    console.log("File has been created");
-});
+      if (err) {
+          console.error(err);
+          return;
+      };
+      console.log("File has been created");
+  });
 });
