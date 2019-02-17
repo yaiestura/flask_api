8e4c6028d7c71ca14717e623c27f0fb194ca07d1
Artemy <magar.art@yandex.ru>
2019-02-17 16:38:07 +0300

    fix discovery script

--- src/backend/utils/discovery.py
+++ src/backend/utils/discovery.py
@@ -5,29 +5,24 @@ import re
 def discovery():
     cameras = []
 
-    try:
-        wsd = WSDiscovery()
-        wsd.start()
-        ret = wsd.searchServices()
-    
-    
-        for index, service in enumerate(ret):
-            address = service.getXAddrs()[0]
+    wsd = WSDiscovery()
+    wsd.start()
+    ret = wsd.searchServices()
 
-            if 'onvif' not in address:
-                continue
+    for index, service in enumerate(ret):
+        address = service.getXAddrs()[0]
 
-                res = re.findall(r'\d+\.\d+\.\d+\.\d+|(?<=:)\d+', address)
-                cam = {
-                    'id': index+1,
-                    'ip': res[0],
-                    'port': res[1] if len(res) > 1 else '80',
-                }
-                cameras.append(cam)
-            
-        wsd.stop()
-    except Exception as e:
-        print 'Discovery problem: %s' % e
+        if 'onvif' not in address:
+            continue
 
+        res = re.findall(r'\d+\.\d+\.\d+\.\d+|(?<=:)\d+', address)
+        cam = {
+            'id': index+1,
+            'ip': res[0],
+            'port': res[1] if len(res) > 1 else '80',
+        }
+        cameras.append(cam)
+
+    wsd.stop()
     print cameras
     return cameras
