34951922f367523172e132c5939a070590271251
Paul Tsvetov <patsvetov@edu.hse.ru>
2019-02-22 23:46:25 +0300

    fix React snapshot URL

--- src/spa/src/components/dashboard/Snapshot.js
+++ src/spa/src/components/dashboard/Snapshot.js
@@ -5,7 +5,7 @@ class Snapshot extends Component {
   render() {
     return(
       <div className="snapshot">        
-        <img className="materialboxed" src={this.props.uri} 
+        <img className="snap" src={`${ this.props.uri }?${ Date.now() }`} 
         onError={ (e) => { e.target.src = "http://www.inspiredbydrive.com/wp-content/uploads/2016/12/no-image.png" }} 
         height="350px" alt="Snapshot"/>
       </div>
