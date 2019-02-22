f497c76bab26308814ee2ee20fd5d01739c56e5a
Paul Tsvetov <patsvetov@edu.hse.ru>
2019-02-23 01:19:09 +0300

    fix bug

--- src/spa/src/components/dashboard/Snapshot.js
+++ src/spa/src/components/dashboard/Snapshot.js
@@ -6,18 +6,15 @@ class Snapshot extends Component {
   handleClick() {
     this.forceUpdate()
   }
-
-  componentWillMount() {
-    <Preloader/>
-  }
-
+  
   render() {
     return(
-      <div className="snapshot">        
+      <div className="snapshot">
+        { !this.props.uri ? <Preloader/> :
         <img className="snap" src={`${ this.props.uri }?${ Date.now() }`} 
         onError={ (e) => { e.target.src = "http://www.inspiredbydrive.com/wp-content/uploads/2016/12/no-image.png" }} 
         onClick={ () => { this.handleClick()}}
-        height="350px" alt="Snapshot"/>
+      height="350px" alt="Snapshot"/> }
       </div>
     )    
   }
