2eb304b7f26f9cd1a0af2e046a3300a5b3f0df78
Paul Tsvetov <patsvetov@edu.hse.ru>
2019-03-03 13:54:26 +0300

    fix labmda in sort

--- src/backend/utils/discovery.py
+++ src/backend/utils/discovery.py
@@ -26,6 +26,6 @@ def discovery():
 
     wsd.stop()
     #Sort cameras
-    cameras = sorted(cameras, key=lambda s: s['ip']: (len(s), re.findall(r'\d+$', s['ip'])))
+    cameras = sorted(cameras, key=lambda x: (len(x), x['ip']))
     print cameras
     return cameras
